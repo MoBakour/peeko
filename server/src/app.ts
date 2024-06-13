d7e51e9f38aa7fedbb7c23703ac69865367672d4
Swordax <swordax.sy@gmail.com>
2024-06-13 15:25:02 +0300

    fix indentation

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,18 +37,18 @@ const API_VERSION = "1.0.1";
 
 // connect to db
 mongoose
-	.connect(process.env.MONGODB_CONNECTION_STRING!, {
-		dbName: "peeko-database",
-	})
-	.then(() => {
-		app.listen(SERVER_PORT, () => {
-			console.log(`Server running on port: ${SERVER_PORT}`);
-			console.log(`Peeko API version: ${API_VERSION}`);
-		});
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+    .connect(process.env.MONGODB_CONNECTION_STRING!, {
+        dbName: "peeko-database",
+    })
+    .then(() => {
+        app.listen(SERVER_PORT, () => {
+            console.log(`Server running on port: ${SERVER_PORT}`);
+            console.log(`Peeko API version: ${API_VERSION}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 // use authenticateUser
 app.use(authenticateUser);
@@ -61,20 +61,20 @@ app.use("/feedback", feedbackController);
 
 // info route
 app.get("/api-info", (req, res) => {
-	res.status(200).json({
-		api_name: "api-peeko",
-		api_version: API_VERSION,
-		api_description: "Peeko social media platform backend API",
-		developer: "MoBakour",
-		developer_contacts: "https://linktr.ee/swordax",
-	});
+    res.status(200).json({
+        api_name: "api-peeko",
+        api_version: API_VERSION,
+        api_description: "Peeko social media platform backend API",
+        developer: "MoBakour",
+        developer_contacts: "https://linktr.ee/swordax",
+    });
 });
 
 // default (404) route
 app.use((req, res) => {
-	res.status(404).json({
-		success: false,
-		error: "404 - UNDEFINED ROUTE",
-		route: req.path,
-	});
+    res.status(404).json({
+        success: false,
+        error: "404 - UNDEFINED ROUTE",
+        route: req.path,
+    });
 });
